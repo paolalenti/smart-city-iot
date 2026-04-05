f4d2e12fb307723c2d8543f625452a9fa100c1be
LiquidIce64 <gilyazov.rm2005@gmail.com>
2026-04-05 18:28:43 +0300

    Added alert engine

---
+++ alert_engine/producer.py
@@ -0,0 +1,33 @@
+import os
+import json
+from confluent_kafka import Producer
+
+KAFKA_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+print(f"DEBUG: Connecting to Kafka at {KAFKA_SERVER}")
+
+conf = {'bootstrap.servers': KAFKA_SERVER}
+producer = Producer(conf)
+
+
+def delivery_report(err, msg):
+    """ Отчет о доставке сообщения (callback) """
+    if err is not None:
+        print(f"Ошибка доставки сообщения: {err}")
+    else:
+        print(f"Сообщение доставлено в {msg.topic()} [{msg.partition()}]")
+
+
+def send_alert(reading: str, high: bool):
+    """Отправляет событие в топик alerts"""
+    payload = {
+        'alert_type': f"{reading}-{'high' if high else 'low'}"
+    }
+
+    producer.produce(
+        topic="alerts",
+        value=json.dumps(payload).encode('utf-8'),
+        callback=delivery_report
+    )
+
+    producer.poll(0)
+    producer.flush()
